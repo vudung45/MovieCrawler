94086c1754a2fdf38b72282c68d7d72820224fb1
vudung45 <vudung45@yahoo.com>
2020-02-20 05:26:34 -0600

    config env for heroku

--- database/config.py
+++ database/config.py
@@ -4,6 +4,9 @@ class Config:
     LOGIN_CREDENTIALS = os.environ["MONGOURI"]
 
 
-with open("./credentials/mongodb.json", "r") as f:
-    settings = json.loads(f.read())
-    Config.LOGIN_CREDENTIALS = settings["loginURL"]
+try:
+    with open("./credentials/mongodb.json", "r") as f:
+        settings = json.loads(f.read())
+        Config.LOGIN_CREDENTIALS = settings["loginURL"]
+except Exception as e:
+    print(e)
